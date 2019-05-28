07cb1a94b6af4e6a27e80cdfe039f66522baa068
volkankahraman <khrmnvolkan@gmail.com>
2019-05-28 03:47:37 +0300

    Sunucu Ayarları yapıldı

--- app.js
+++ app.js
@@ -1,17 +1,60 @@
-const rp = require('request-promise');
-var CronJob = require('cron').CronJob;
+var rp = require('request-promise');
+var express = require('express');
+const nodeMailer = require('nodemailer');
 
-const api = 'b4fee630-2e72-4a90-9ff0-cf91d392c6f6';
+var app = express();
+
+var port = process.env.PORT || 8000;
+app.listen(port, () => console.log(`Sunucu ${port} portunda çalışıyor!`))
+
+
+const api = process.env.API;
+
+let transporter = nodeMailer.createTransport({
+  host: 'smtp.zoho.com',
+  port: 465,
+  secure: true, //true for 465 port, false for other ports
+  auth: {
+    user: process.env.EMAIL,
+    pass: process.env.PASSWORD
+  }
+  
+});
+
 
 var TronTry;
+
+function GetDateInFormat(date) {
+  return date.getDate() + "-" + (date.getMonth() + 1) + "-" + date.getFullYear() + " " + (date.getHours()<10?'0':'') + date.getHours() + ":" + (date.getMinutes()<10?'0':'') + date.getMinutes() + ":" + (date.getSeconds()<10?'0':'') + date.getSeconds();
+}
+
+function sendEmail(txtResponse){
+  let mailOptions = {
+    from: 'Invesment Tool <khrmnvolkan@zoho.com>', // sender address
+    to: 'khrmnvolkan@gmail.com', // list of receivers
+    subject: 'Yatırım Değeri ' + GetDateInFormat(new Date()), // Subject line
+    html: txtResponse // html body
+  };
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log(error);
+      res.send(500);
+    } else {
+      console.log("email gönderildi");
+      console.log(info);
+      res.status(200).send({success: true});
+    }
+    
+  });
+
+}
 
 
 const requestOptions = {
   method: 'GET',
-  uri: 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest',
+  uri: 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest',
   qs: {
-    'start': '11',
-    'limit': '1',
+    'id': '1958',
     'convert': 'TRY'
   },
   headers: {
@@ -20,16 +63,21 @@ const requestOptions = {
   json: true,
   gzip: true
 };
-
 rp(requestOptions).then(response => {
-    TronTry = response.data[0].quote.TRY.price;
-    console.log("Yatırım Değeri 1256 Tron Tl Karşılığı : "+ 1256* TronTry.toFixed(2)+' Tldir');
+  TronTry = Object.values(response.data)[0].quote.TRY.price * 1256;
+  var txtResponse = "<h1>1256 Tron Tl Karşılığı :  "+TronTry.toFixed(2) + ' Tldir</h1>';
+  
+    
+  app.get('/',function(req,res){
+  //sendEmail(txtResponse);
+    res.writeHead(200, {
+      "Content-Type": "text/html; charset=utf-8"
+    });
+    console.log(txtResponse);
+    res.write(txtResponse, 'utf8');
+    res.end();
+    });
 
 }).catch((err) => {
   console.log('API call error:', err.message);
 });
-
-
-new CronJob('5 * * * * *', function() {
-  console.log('You will see this message every second');
-}, null, true, 'America/Los_Angeles');
